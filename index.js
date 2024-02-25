1e568f264f8b0b05a073f5746557ddd4ed0a1edf
Vardaan Aggarwal <98269336+VardaanAggarwal@users.noreply.github.com>
2024-02-25 10:43:02 +0000

    comments

--- index.js
+++ index.js
@@ -29,4 +29,22 @@ app.get("/products/:categoryName", (req, res) => {
   // name, price, description
 })
 
+app.get("/product/:productId", (req, res) => {
+  // write the logic to fetch the product by it's id
+  // fetch name, price, description, quantity, categories
+})
+
+app.post("/cart/:productId", (req, res) => {
+  // write the logic to add the product to the user's cart (fecth the user's detail through authentication)
+})
+
+app.post("/order/:productId", (req, res) => {
+  // write the logic for purchasing a product and create a order for the user with the product id
+  // set the status by default to false
+  // handle the race condition where 2 users might try to buy and place order for the same product
+  // which will result in concurrency issues
+  // reduce the quantity of the product by the amount of the product user bought
+  // check before placing order if the product quantity is here or not to place the order
+})
+
 
